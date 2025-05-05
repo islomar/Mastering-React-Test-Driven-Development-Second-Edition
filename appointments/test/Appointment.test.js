a3f7df5a6727da17e9b7d63153d13f495c000ebe
Isidro Lopez <islomar@gmail.com>
2025-05-05 22:45:05 +0200

    Improve the first failing test

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,5 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 describe("Appointment", () => {
     it("renders the customer first name", () => {
+        const customer = { firstName: "Ashley" };
+        const component = (
+            <Appointment customer={customer} />
+        );
+        const container = document.createElement("div");
+        document.body.appendChild(container);
+
+        ReactDOM.createRoot(container).render(component);
+
         expect(document.body.textContent).toContain("Ashley");
     });
 });
