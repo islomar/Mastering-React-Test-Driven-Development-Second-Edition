918cd3837441695919b2cee6f55327a29e699e87
Isidro Lopez <islomar@gmail.com>
2025-05-06 13:08:43 +0200

    Show list of appointments in a basic way

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -4,6 +4,14 @@ export const Appointment = ({ customer }) => (
     <div>{customer.firstName}</div>
 );
 
-export const AppointmentsDayView = () => (
-    <div id="appointmentsDayView"></div>
+export const AppointmentsDayView = ({ appointments }) => (
+    <div id="appointmentsDayView">
+        appointments.map((appointment, index) => (
+            <ol>
+                {appointments.map(appointment => (
+                    <li key={appointment.startsAt} />
+                ))}
+            </ol>
+        );
+    </div>
 );
