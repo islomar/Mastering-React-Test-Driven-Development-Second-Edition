6848ad01519f419705ffb45ca79fcc1051a2e889
Isidro Lopez <islomar@gmail.com>
2025-05-06 13:18:44 +0200

    Show list of appointments with hour and minute

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+const appointmentTimeOfDay = (startsAt) => {
+    const [h, m] = new Date(startsAt)
+        .toTimeString()
+        .split(":");
+    return `${h}:${m}`;
+}
+
 export const Appointment = ({ customer }) => (
     <div>{customer.firstName}</div>
 );
@@ -9,7 +16,9 @@ export const AppointmentsDayView = ({ appointments }) => (
         appointments.map((appointment, index) => (
             <ol>
                 {appointments.map(appointment => (
-                    <li key={appointment.startsAt} />
+                    <li key={appointment.startsAt}>
+                        {appointmentTimeOfDay(appointment.startsAt)}
+                    </li>
                 ))}
             </ol>
         );
