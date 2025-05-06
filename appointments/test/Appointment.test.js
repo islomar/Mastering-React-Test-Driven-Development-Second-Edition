88346024559ee4edde6a0903ca20d0aaacea6372
Isidro Lopez <islomar@gmail.com>
2025-05-06 11:57:07 +0200

    Refactor tests: extract document action to beforeEach

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -4,13 +4,17 @@ import ReactDOM from "react-dom/client";
 import { Appointment } from "../src/Appointment";
 
 describe("Appointment", () => {
+    let container;
+    beforeEach(() => {
+        container = document.createElement("div");
+        document.body.replaceChildren(container);
+    });
+    
     it("renders the customer first name", () => {
         const customer = { firstName: "Ashley" };
         const component = (
             <Appointment customer={customer} />
         );
-        const container = document.createElement("div");
-        document.body.replaceChildren(container);
 
         act(() =>
             ReactDOM.createRoot(container).render(component)
@@ -24,8 +28,6 @@ describe("Appointment", () => {
         const component = (
             <Appointment customer={customer} />
         );
-        const container = document.createElement("div");
-        document.body.replaceChildren(container);
 
         act(() =>
             ReactDOM.createRoot(container).render(component)
