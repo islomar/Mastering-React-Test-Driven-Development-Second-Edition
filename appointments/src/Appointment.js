97e788eca3d3284072c8f6c949d370da8480765a
Isidro Lopez <islomar@gmail.com>
2025-05-06 13:26:44 +0200

    Show text when no appointments exist

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -22,5 +22,10 @@ export const AppointmentsDayView = ({ appointments }) => (
                 ))}
             </ol>
         );
+        {appointments.length === 0 ? (
+            <p>There are no appointments scheduled for today.</p>
+        ) : (
+            <Appointment {...appointments[0]} />
+        )}
     </div>
 );
