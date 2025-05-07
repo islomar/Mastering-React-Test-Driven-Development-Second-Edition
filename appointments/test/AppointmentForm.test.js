8bf30ef22e4db882e5a6bd8219fe02db2d6abf7b
Isidro Lopez <islomar@gmail.com>
2025-05-07 12:52:01 +0200

    Create basic AppointmentForm component

---
+++ appointments/test/AppointmentForm.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import {
+    initializeReactContainer,
+    render,
+    field,
+    form,
+} from "./reactTestExtensions";
+
+import { AppointmentForm } from "../src/AppointmentForm";
+
+describe("AppointmentForm", () => {
+    beforeEach(() => {
+        initializeReactContainer();
+    });
+
+    it("renders a form", () => {
+        render(<AppointmentForm />);
+        expect(form()).not.toBeNull();
+    });
+
+    describe("service field", () => {
+        it("renders as a select box", () => {
+            render(<AppointmentForm />);
+
+            expect(field("service")).not.toBeNull();
+            expect(field("service").tagName).toEqual("SELECT");
+        });
+    });
+});
